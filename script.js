795421e022b5afb6e9929a4ecf6fc0eae5ef2d80
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-10-29 13:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ function strip = (touristSpotsName){
 	return touristSpotsName.replace(/^(a |the |an )/i,'').trim();
 }
 const sortedBand = touristSpots.sort((a,b)=>strip(a)>strip(b)?1:-1);
-Document.querySelector(#bands).innerText = sortedBand.map(band=>'<li>${band}</li>').join('');
+// Document.querySelector(#bands).innerText = sortedBand.map(band=>'<li>${band}</li>').join('');
