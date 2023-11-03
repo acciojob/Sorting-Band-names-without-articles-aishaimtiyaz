c2487c900f97a8445d67296c7435fd10cfd72794
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-03 18:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your code here
-function strip = (touristSpotsName){
+function strip (touristSpotsName){
 	return touristSpotsName.replace(/^(a |the |an )/i,'').trim();
 }
 const sortedBand = touristSpots.sort((a,b)=>strip(a)>strip(b)?1:-1);
